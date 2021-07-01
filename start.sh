ad72d786b3f5913dcacaf454004455aea3c2235c
sagirisayang <64939358+sagirisayang@users.noreply.github.com>
2021-07-01 12:27:36 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,9 @@
-TOKEN_PICKLE_URL=1
-ACCOUNTS_ZIP_URL=2
-
 if [[ -n $TOKEN_PICKLE_URL ]]; then
-	echo "token"
+   echo "token"
 fi
 
 if [[ -n $ACCOUNTS_ZIP_URL ]]; then
-	echo "accounts"
+   echo "accounts"
 fi
+
+echo "success"
